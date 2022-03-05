cd770f72fe015d0877d8ac573dd3b0cb521a1e5d
n1se <theodoregx@gmail.com>
2022-03-05 13:38:02 -0500

    Add files via upload

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,1 +1,2 @@
-
+#!/bin/sh
+xrandr --output DisplayPort-0 --set TearFree on --mode 1920x1080 --pos 3840x0 --rotate normal --output DisplayPort-1 --set TearFree on --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --set TearFree on --primary --mode 1920x1080 --pos 1920x0 --rotate normal
