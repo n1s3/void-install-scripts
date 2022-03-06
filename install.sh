2c6ade3c214677fd137bfb2806dde51ad061a98c
n1se <theodoregx@gmail.com>
2022-03-05 19:29:00 -0500

    Update install.sh
    comment

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ then
 	exit	
 fi
 
+#installs git, it's probably installed already but why not. Now it's certainly installed.
 mkdir -p $HOME/Downloads && cd $HOME/Downloads
 xbps-install --yes git && git clone https://github.com/n1s3/void-install-scripts.git
 PKGS=$(cat $HOME/Downloads/void-install-scripts/pkgs.list)
