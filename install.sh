2e6a00a02851989961eaf914dafcb280c54adfe3
n1se <theodoregx@gmail.com>
2022-03-08 18:03:15 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,19 +8,30 @@ do
 	xbps-install --yes $PKG
 done
 
-echo -e 'Installing vim-plug... \n'
+echo -e '\nInstalling vim-plug...'
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo -e 'Installing dwm... \n'
+echo -e '\nInstalling dwm...'
 cd dwm && make clean install
-echo -e 'Installing dmenu... \n'
+echo -e '\nInstalling dmenu...'
 cd .. && cd dmenu && make clean install
-cd ..
+cd #go home
 
-echo -e 'Migrating dot files and configs... '
+echo -e '\nMigrating dot files and configs... '
 
 for dotf in $DOTFILES
 do
-	cp -r dotf
+	cp -r $dotf ~/
 done
+
+clear
+echo '\nInstall video drivers? [y/N]
+read ans
+clear
+if [ $ans -eq 'y' ]
+	echo -e '\nInstalling amd drivers...'
+	xbps-install --yes xf86-video-vesa
+	xbps-install --yes xf86-video-amdgpu
+fi
+echo -e '\nDone, consider restarting asap.'
